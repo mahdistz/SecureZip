65b975f440e5aa1c494fda5e7b109697a377f2ea
Mahdis Taghizadeh <mahdis.taghizadeh1376@gmail.com>
2024-04-30 16:55:26 +0330

    add project files

---
+++ README.md
@@ -0,0 +1,69 @@
+## SecureZip: Create Password-protected Zip Files
+
+SecureZip is a Python program that allows users to create encrypted ZIP archives with password protection from files or folders.
+
+**Features:**
+
+* Supports both individual files and directories.
+* Allows you to choose a password or generate a random one.
+* Logs the creation of the zip file and password to the console.
+
+**Requirements:**
+
+* Python 3 (tested with 3.x versions)
+* `pyzipper` library
+
+**Setting Up a Virtual Environment:**
+
+It's highly recommended to use a virtual environment to isolate project dependencies and avoid conflicts with other
+Python installations on your system. Here's how to set up a virtual environment using `venv` (available in Python 3.3+):
+
+1. Open your terminal or command prompt.
+2. Navigate to the directory where you want to create the virtual environment.
+3. Run the following command to create a virtual environment named `venv`:
+
+```bash
+python -m venv venv
+```
+
+4. Activate the virtual environment:
+
+    - **Windows:**
+       ```bash
+       venv\Scripts\activate
+       ```
+    - **macOS/Linux:**
+       ```bash
+       source venv/bin/activate
+       ```
+
+**Install:**
+
+Once you have activated the virtual environment, you can install the required dependencies:
+
+1. Ensure you're in the activated virtual environment (check the terminal prompt, which should indicate the virtual
+   environment name).
+2. Run the following command:
+
+```bash
+pip install -r requirements.txt
+```
+
+**Usage:**
+
+1. **Run the script:**
+    * Navigate to the directory containing the script.
+    * Run the script with `python main.py`. Make sure you have activated the virtual environment.
+
+2. **Enter path and password:**
+    * The script will prompt you for the file/folder path.
+    * Enter the full path to the file or directory you want to zip.
+    * You'll then be prompted for a password.
+    * Enter your desired password (leave blank for a random one).
+
+3. **Success message:**
+    * Upon successful zip creation, the script will display the location of the created zip file and the password.
+
+**Contributing:**
+
+Feel free to fork the repository and submit pull requests with improvements or bug fixes.
---
+++ main.py
@@ -0,0 +1,65 @@
+import os
+from pathlib import Path
+
+import pyzipper
+import secrets
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(levelname)s] [%(asctime)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    handlers=[
+        logging.StreamHandler()
+    ]
+)
+BASE_DIR = Path(__file__).resolve().parent
+
+
+def create_zip_with_password(item_path, password, zip_files_path):
+    try:
+        if os.path.isfile(item_path):
+            item_name = os.path.splitext(os.path.basename(item_path))[0]
+            zip_file_path = os.path.join(zip_files_path, f"{item_name}.zip")
+            with pyzipper.AESZipFile(zip_file_path, 'w', compression=pyzipper.ZIP_STORED,
+                                     encryption=pyzipper.WZ_AES) as zf:
+
+                zf.setpassword(bytes(password, 'utf-8'))
+                zf.write(item_path, os.path.basename(item_path))
+            logging.info(f"Zip file created at: {zip_file_path}")
+
+        elif os.path.isdir(item_path):
+            zip_file_path = os.path.join(zip_files_path, f"{os.path.basename(item_path)}.zip")
+            with pyzipper.AESZipFile(zip_file_path, 'w', compression=pyzipper.ZIP_STORED,
+                                     encryption=pyzipper.WZ_AES) as zf:
+
+                zf.setpassword(bytes(password, 'utf-8'))
+                for root, dirs, files in os.walk(item_path):
+                    for file in files:
+                        file_path = os.path.join(root, file)
+                        zf.write(file_path, os.path.relpath(file_path, item_path))
+
+            logging.info(f"Zip file created at: {zip_file_path}")
+        logging.info(f"Password: {password}")
+
+    except Exception as e:
+        logging.error(f'An error occurred: {e}')
+
+
+def main():
+    item_path = input("Enter the file or folder path: ").strip().strip('"').replace("\\", "/")
+    user_password = input("Enter a password for the zip file (leave blank to use a random password): ").strip()
+
+    zip_files_path = os.path.join(BASE_DIR, 'zip_files')
+    if not os.path.exists(zip_files_path):
+        os.makedirs(zip_files_path)
+
+    if not user_password:
+        password = secrets.token_urlsafe(16)
+        create_zip_with_password(item_path=item_path, password=password, zip_files_path=zip_files_path)
+    else:
+        create_zip_with_password(item_path=item_path, password=user_password, zip_files_path=zip_files_path)
+
+
+if __name__ == "__main__":
+    main()
